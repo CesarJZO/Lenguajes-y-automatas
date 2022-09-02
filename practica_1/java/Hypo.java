dc85230026767aed385d4e128cbc7acc01cb9927
CesarJZO <cesarjzo@outlook.com>
2022-09-02 15:06:39 -0500

    Algoritmo de hipotenusa

---
+++ practica_1/java/Hypo.java
@@ -0,0 +1,21 @@
+package practica_1.java;
+
+import java.util.Scanner;
+import java.lang.String;
+public class Hypo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Inserte los valores de los catetos");
+        System.out.print("x: ");
+        double x = scanner.nextDouble();
+        System.out.print("y: ");
+        double y = scanner.nextDouble();
+
+        double h = Math.sqrt(x * x + y * y);
+
+        System.out.println("h: " + h);
+
+        scanner.close();
+    }
+}
